5bbfdabb3e37aa44387bd1160a2be4f3f4e35acd
Ivan <ivantm24@gmail.com>
2020-01-28 19:13:50 -0400

    implementa funciones cvrea min

--- clase3_minunit_cuadratica/CuadraticaLib.c
+++ clase3_minunit_cuadratica/CuadraticaLib.c
@@ -3,10 +3,18 @@
 //
 
 #include "CuadraticaLib.h"
+#include <math.h>
+
+float calc_delta(float a, float b, float c);
+
+float calc_delta(float a, float b, float c){
+    return sqrt(b*b - 4 * a *c);
+}
 
 float cuadratica_pos(float a, float b, float c){
-
+    return (-b + calc_delta(a, b, c))/2.0*a;
 }
 float cuadratica_neg(float a, float b, float c){
-
+    return (-b - calc_delta(a, b, c))/2.0*a;
 }
+

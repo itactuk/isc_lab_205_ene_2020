babb609b929a9cf4dc3e301815fee2976f4372e1
Ivan <ivantm24@gmail.com>
2020-01-28 19:04:23 -0400

    termine minunit

--- clase3_minunit_cuadratica/CuadraticaMinunit.c
+++ clase3_minunit_cuadratica/CuadraticaMinunit.c
@@ -19,8 +19,19 @@ MU_TEST(test_prueba_c0) {
     mu_assert_double_eq(0.0, x2);
 }
 
+MU_TEST(test_prueba_b0) {
+    float a = 1.0, b = 0.0, c = -4;
+    // x^2 - 4 = 0; x1 = 2, x2 = -2
+    float x1 = cuadratica_pos(a, b, c);
+    float x2 = cuadratica_neg(a, b, c);
+
+    mu_assert_double_eq(2.0, x1);
+    mu_assert_double_eq(-2.0, x2);
+}
+
 MU_TEST_SUITE(test_suite) {
-    MU_RUN_TEST(test_NombrePrueba);
+    MU_RUN_TEST(test_prueba_c0);
+    MU_RUN_TEST(test_prueba_b0);
 }
 
 int main(int argc, char *argv[]) {

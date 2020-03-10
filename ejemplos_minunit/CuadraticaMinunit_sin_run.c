ca73984303ad44757607fe431b6f71ed73b44467
Ivan <ivantm24@gmail.com>
2020-03-09 20:22:10 -0400

    agrega ejemplos minunit

---
+++ ejemplos_minunit/CuadraticaMinunit_sin_run.c
@@ -0,0 +1,38 @@
+//
+// Created by Ivan Tactuk on 1/28/2020.
+//
+
+#include <minunit.h>
+
+#include "CuadraticaLib.h"
+
+#define MINUNIT_EPSILON 1E-1
+
+MU_TEST(test_prueba_c0) {
+    float a = 1.0, b = -2.0, c = 0;
+    float x1 = cuadratica_pos(a, b, c);
+    float x2 = cuadratica_neg(a, b, c);
+
+    mu_assert_double_eq(2.0, x1);
+    mu_assert_double_eq(0.0, x2);
+}
+
+MU_TEST(test_prueba_b0) {
+    float a = 1.0, b = 0.0, c = -4;
+    // x^2 - 4 = 0; x1 = 2, x2 = -2
+    float x1 = cuadratica_pos(a, b, c);
+    float x2 = cuadratica_neg(a, b, c);
+
+    mu_assert_double_eq(2.0, x1);
+    mu_assert_double_eq(-2.0, x2);
+}
+
+MU_TEST_SUITE(test_suite) {
+
+}
+
+int main(int argc, char *argv[]) {
+    MU_RUN_SUITE(test_suite);
+    MU_REPORT();
+    return 0;
+}

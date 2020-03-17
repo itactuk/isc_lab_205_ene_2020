17bf8500b420bfc0be9a8d661d52511353f45b01
Ivan <ivantm24@gmail.com>
2020-03-17 18:55:47 -0400

    ejemplo archivos binarios final

--- clase7_archivos/archivos_main.c
+++ clase7_archivos/archivos_main.c
@@ -2,8 +2,14 @@
 // Created by Ivan's PC on 3/17/2020.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
 
-// int main(int n, char ** args)
-int main(){
+ int main(int n, char ** args){
+    printf("Hello world %d", n);
+     for (int i = 0; i < n; ++i) {
+         printf("\nMi primer parametro es: %s", args[i]);
+     }
 
+     system("PAUSE");
 }

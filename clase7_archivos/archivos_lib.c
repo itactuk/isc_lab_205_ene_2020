73f97a857403e9fa08ede4a3d493c9720f54c470
Ivan <ivantm24@gmail.com>
2020-03-17 18:39:02 -0400

    ejemplo archivos binarios

--- clase7_archivos/archivos_lib.c
+++ clase7_archivos/archivos_lib.c
@@ -16,11 +16,36 @@ void guardar_perros(char * archivo, ARREGLO_PERRO arreglo_perros){
 
     for (int i = 0; i < n; ++i) {
         fwrite(&perros[i].n_amigos, sizeof(int), 1, p);
+        // Aquí hay un problema con los amigos de los amigos del perro
+        // ya que los punteros no se están guardando
+        // Se podría resolver con recursividad,
+        // pero estamos asumiendo que los amigos de los amigos no tienen amigos
         fwrite(perros[i].amigos, sizeof(PERROS), perros[i].n_amigos, p);
     }
     fclose(p);
 }
 
 ARREGLO_PERRO leer_perros(char * archivo){
+    ARREGLO_PERRO arreglo_perros;
+    int n;
+    PERROS * perros;
+    FILE * p = fopen(archivo, "rb"); // rb para leer un archivo
 
+    fread(&n, sizeof(int), 1, p);
+    perros = (PERROS*) malloc(n* sizeof(PERROS));
+    fread(perros, sizeof(PERROS), n, p);
+
+    for (int i = 0; i < n; ++i) {
+        fread(&perros[i].n_amigos, sizeof(int), 1, p);
+        // Aquí hay un problema con los amigos de los amigos del perro
+        // ya que los punteros no se están guardando
+        // Se podría resolver con recursividad,
+        // pero estamos asumiendo que los amigos de los amigos no tienen amigos
+        perros[i].amigos = (PERROS*) calloc(perros[i].n_amigos, sizeof(PERROS));
+        fread(perros[i].amigos, sizeof(PERROS), perros[i].n_amigos, p);
+    }
+    fclose(p);
+    arreglo_perros.n = n;
+    arreglo_perros.perros = perros;
+    return arreglo_perros;
 }
